02cadf39c5892dd09c7c7894a5fe543840c9ed0f
Brian Gerez <bgerez@localhost.localdomain>
2016-11-19 20:29:13 -0300

    Mejora del login, ademas de mandar por json el personaje creado al servidor

--- cliente/src/main/java/cliente/PantallaUsuario.java
+++ cliente/src/main/java/cliente/PantallaUsuario.java
@@ -24,6 +24,7 @@ import main.java.mergame.skill.hechizo.HechizoFuego;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.HashMap;
@@ -46,7 +47,24 @@ public class PantallaUsuario extends JFrame {
 	private Map <String, Personaje> personajes;
 	private Map <String, EsDeCasta> casta;
 	
-	public PantallaUsuario(Principal pantallaPrincipal) {
+	//ATRIBUTOS PARA LA CONEXION//
+	private Socket socket;
+	private Scanner entradaDatos;
+	private PrintWriter salidaDatos;
+		
+	public PantallaUsuario(Socket socket) throws IOException {
+		
+		this.pantallaPrincipal = pantallaPrincipal;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 699, 396);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		this.socket = socket;
+		entradaDatos = new Scanner(socket.getInputStream());
+		salidaDatos = new PrintWriter(socket.getOutputStream());
 		
 		//INSTANCIAS DE LOS MAPAS, QUE CONTIENE TODOS LOS PERSONAJES Y LAS CASTAS
 		personajes = new HashMap<>();
@@ -59,49 +77,46 @@ public class PantallaUsuario extends JFrame {
 		casta.put("Mago", new Mago());
 		casta.put("Guerrero", new Guerrero());
 		
-		//////////////////////////////////////////////////////////////////////////
+		//COMPONENTES NO MODIFICABLES//		
+		JLabel lblSeleccionPersonaje = new JLabel("Seleccioná personaje:");
+		lblSeleccionPersonaje.setBounds(71, 87, 234, 15);
+		contentPane.add(lblSeleccionPersonaje);
 		
+		JLabel lblAhoraSeleccionUna = new JLabel("Ahora seleccioná una casta:");
+		lblAhoraSeleccionUna.setBounds(71, 144, 232, 15);
+		contentPane.add(lblAhoraSeleccionUna);
 		
-		this.pantallaPrincipal = pantallaPrincipal;
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 699, 396);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		JLabel lblyQueTal = new JLabel("¿Y que tal si le agregas un nombre?");
+		lblyQueTal.setBounds(71, 220, 259, 15);
+		contentPane.add(lblyQueTal);
 		
-		
-		JLabel labelNombreUsuario = new JLabel(pantallaPrincipal.getTextFieldUsuario());
-		labelNombreUsuario.setHorizontalAlignment(SwingConstants.CENTER);
-		labelNombreUsuario.setBounds(308, 26, 70, 38);
-		contentPane.add(labelNombreUsuario);
+		labelErrorTipeo = new JLabel("Completá el nombre loco!");
+		labelErrorTipeo.setBounds(320, 249, 203, 15);
+		labelErrorTipeo.setForeground(Color.RED);
+		contentPane.add(labelErrorTipeo);
+		labelErrorTipeo.setVisible(false);
 		
 		JLabel lblBienvenido = new JLabel("Bienvenido");
 		lblBienvenido.setHorizontalAlignment(SwingConstants.CENTER);
 		lblBienvenido.setBounds(308, 11, 70, 15);
 		contentPane.add(lblBienvenido);
 		
+		//////////////////////////////////////////////////////////////////////////				
+		
 		JButton btnCrearNuevoPersonaje = new JButton("Ingresá a tu mundo, a luchar!");
 		btnCrearNuevoPersonaje.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if(!textField.getText().equals("")){
-					conectarConServidor();
+					enviarPersonaje();
 				}
 				else{
 					labelErrorTipeo.setVisible(true);
 				}
 			}
 		});
-		
-
-		
 		btnCrearNuevoPersonaje.setBounds(389, 304, 259, 25);
 		contentPane.add(btnCrearNuevoPersonaje);
-		
-		JLabel lblSeleccionPersonaje = new JLabel("Seleccioná personaje:");
-		lblSeleccionPersonaje.setBounds(71, 87, 234, 15);
-		contentPane.add(lblSeleccionPersonaje);
-		
+
 		comboBoxPersonajes = new JComboBox();
 		comboBoxPersonajes.setBounds(71, 108, 146, 24);
 		contentPane.add(comboBoxPersonajes);
@@ -110,108 +125,45 @@ public class PantallaUsuario extends JFrame {
 		comboBoxPersonajes.addItem("Humano");
 		comboBoxPersonajes.addItem("Orco");
 		
-		JLabel lblAhoraSeleccionUna = new JLabel("Ahora seleccioná una casta:");
-		lblAhoraSeleccionUna.setBounds(71, 144, 232, 15);
-		contentPane.add(lblAhoraSeleccionUna);
-		
 		comboBoxCastas = new JComboBox();
 		comboBoxCastas.setBounds(71, 163, 156, 24);
 		contentPane.add(comboBoxCastas);
 		comboBoxCastas.addItem("Guerrero");
 		comboBoxCastas.addItem("Mago");
 		
-		JLabel lblyQueTal = new JLabel("¿Y que tal si le agregas un nombre?");
-		lblyQueTal.setBounds(71, 220, 259, 15);
-		contentPane.add(lblyQueTal);
-		
-		
 		textField = new JTextField(); //acá va el nombre del personaje
 		textField.setBounds(73, 247, 217, 19);
-		
 		contentPane.add(textField);
-		
-		labelErrorTipeo = new JLabel("Completá el nombre loco!");
-		labelErrorTipeo.setBounds(320, 249, 203, 15);
-		labelErrorTipeo.setForeground(Color.RED);
-		contentPane.add(labelErrorTipeo);
-		labelErrorTipeo.setVisible(false);
-		
-		
 	}
 	
 	// compruebo que el usuario complete el nombre del personaje.
-	public void conectarConServidor(){
+	public void enviarPersonaje(){
 		if (!this.textField.getText().equals("")) {
-			
-			String server = "127.0.0.1";
 	        ObjectMapper mapper = new ObjectMapper();
+	        Personaje personaje = this.personajes.get((String)this.comboBoxPersonajes.getSelectedItem());
 	        
 	        try {
-	            final int PORT = 5553;
-	            Socket socket = new Socket(server, PORT);
-	            
-	            System.out.println("Te conectaste a: " + server);
-	            System.out.println("Hablá todo lo que quieras");
-	            
-	            
-	            /*PROBANDO INTERFAZ*/
-	            
-	            int mundo = Integer.valueOf((String) pantallaPrincipal.getComboBox().getSelectedItem());
-	            
-	            User user = new User(mundo, this.textField.getText() );
-	            
-	            String jsonInString = mapper.writeValueAsString(user);
+	        	String jsonInString = mapper.writeValueAsString(personaje);
+	            salidaDatos.println("Nuevo Personaje|"+jsonInString); // LE ENVIO EL MENSAJE DE SALA Y NICKNAME
+	            salidaDatos.flush();
 
-	            PrintWriter out = new PrintWriter(socket.getOutputStream()); //OBTENGO EL CANAL DE SALIDA DEL SOCKET HACIA EL SERVIDOR
-	            out.println(jsonInString); // LE ENVIO EL MENSAJE DE SALA Y NICKNAME
-
-	            out.flush();
-	            
-	            //INSTANCEANDO UN PERSONAJE DENTRO DE MI CLIENTE Y MANDARLO AL SERVIDOR
-	            
-	            Personaje elFerra = this.personajes.get((String)this.comboBoxPersonajes.getSelectedItem());
-	            
-	            ///FIN INSTANCIA PERSONAJE////////////////////////////////////////////////////////////////////
-
-	            //INSTANCIA DEL CLIENTE, DONDE SE MANDA MSJ CON EL SERVIDOR
-	            ClientThread newClient = new ClientThread(socket);
-	            Thread thread = new Thread(newClient);
-	            thread.start();
-	            
-	           
-	            //SE CREA ESTE BOTON DESPUES DE INSTANCEAR EL PERSONAJE, Y LO QUE HACE ES MANDAR AL SERVIDOR LA SALUD DE ELFERRA
-                JButton btnGetSalud = new JButton("mostra tu salud!");
-                btnGetSalud.addActionListener(new ActionListener() {
-        			public void actionPerformed(ActionEvent arg0) {
-        				String mensaje = "";
-        					mensaje = String.valueOf(elFerra.getSalud());
-        					newClient.enviarDatos(mensaje);
-        			}
-        		});
-                btnGetSalud.setBounds(300, 250, 200, 25);
-        		contentPane.add(btnGetSalud);
-        		
-        		//// FIN DE BOTON DE MANDAR SALUD ///
-                
-        		
-        		/* ESTE WHILE ES PARA MANDAR VARIOS MENSAJES, ES DECIR QUE PERTENECIA AL CHAT, NO LO BORRÉ PORQUE SEGURO SIRVE
-                 String textoTeclado = "";
-	            while (!textoTeclado.equals("fin")) { //MIENTRAS NO ESCRIBA FIN PODRE ENVIAR LOS MENSAJES QUE QUIERA
-	                Scanner bufferDeTeclado = new Scanner(System.in);
-	                textoTeclado = bufferDeTeclado.nextLine();
-	                
-	                if (textoTeclado.equals("fin")) { // SI ESCRIBO FIN DESCONECTA TODO
-	                    newClient.desconectar();
-	                    bufferDeTeclado.close();
-	                } else {
-	                    newClient.enviarDatos(textoTeclado);
-	                }
-	            }*/
+//	            //SE CREA ESTE BOTON DESPUES DE INSTANCEAR EL PERSONAJE, Y LO QUE HACE ES MANDAR AL SERVIDOR LA SALUD DE ELFERRA
+//                JButton btnGetSalud = new JButton("mostra tu salud!");
+//                btnGetSalud.addActionListener(new ActionListener() {
+//        			public void actionPerformed(ActionEvent arg0) {
+//        				String mensaje = "";
+//        					mensaje = String.valueOf(personaje.getSalud());
+//        					newClient.enviarDatos(mensaje);
+//        			}
+//        		});
+//                btnGetSalud.setBounds(300, 250, 200, 25);
+//        		contentPane.add(btnGetSalud);
+//        		
+//        		//// FIN DE BOTON DE MANDAR SALUD ///
 
 	        } catch (Exception e) {
 	        	System.out.println(e.getMessage());
 	        }
-	    
 			//dispose();
 		}else{
 			labelErrorTipeo.setVisible(true);
--- servidor/src/main/java/servidor/Server.java
+++ servidor/src/main/java/servidor/Server.java
@@ -2,6 +2,7 @@ package servidor;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -24,35 +25,52 @@ public class Server {
                 Socket socket = server.accept();
 
                 Scanner sc = new Scanner(socket.getInputStream());
-                String input = sc.nextLine();
-
-                User user = mapper.readValue(input, User.class);
+                String [] mundoUsuPass = sc.nextLine().split(" ");
+                int numMundo = Integer.parseInt(mundoUsuPass[0]);
+                String usuario = mundoUsuPass[1];
+                String password = mundoUsuPass[2];
+                
+                if(usuario.equals("test") && password.equals("test")){
+                	System.out.println("Syso de servidor: conectaste bien");
+                	Socket tempSocket = socket;
+                	PrintWriter tempOut = new PrintWriter(tempSocket.getOutputStream());
+                	tempOut.println("Datos Correctos");
+                	tempOut.flush();
 
                 ServerThread chat;
-
-                switch (user.getSala()) {
+                switch (numMundo) {
                     case 1:
                         listaDeConexionesSala1.add(socket);
-                        chat = new ServerThread(socket, listaDeConexionesSala1, user.getNombre());
+                        chat = new ServerThread(socket, listaDeConexionesSala1, usuario);
                         Thread nuevoProcesoParalelo1 = new Thread(chat);
                         nuevoProcesoParalelo1.start();
                         break;
                     case 2:
                         listaDeConexionesSala2.add(socket);
-                        chat = new ServerThread(socket, listaDeConexionesSala2, user.getNombre());
+                        chat = new ServerThread(socket, listaDeConexionesSala2, usuario);
                         Thread nuevoProcesoParalelo2 = new Thread(chat);
                         nuevoProcesoParalelo2.start();
                         break;
                     case 3:
                         listaDeConexionesSala3.add(socket);
-                        chat = new ServerThread(socket, listaDeConexionesSala3, user.getNombre());
+                        chat = new ServerThread(socket, listaDeConexionesSala3, usuario);
                         Thread nuevoProcesoParalelo3 = new Thread(chat);
                         nuevoProcesoParalelo3.start();
                         break;
                     default:
                         break;
                 }
-                System.out.println("Client conectado a Sala " + user.getSala() + " desde: " + socket.getLocalAddress().getHostName());
+                System.out.println("Client conectado a Sala " + numMundo + " desde: " + socket.getLocalAddress().getHostName());
+            }
+            else{
+            	System.out.println("Seras desconectado");
+                	Socket tempSocket = socket;
+                	PrintWriter tempOut = new PrintWriter(tempSocket.getOutputStream());
+                	tempOut.println("Datos Incorrectos");
+                	tempOut.flush();
+                	socket.close();
+                }
+                	
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
